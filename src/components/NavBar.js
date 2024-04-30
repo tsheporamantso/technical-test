bb225147ff2cc16e827dce823e92a6c70cf09b13
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-30 17:23:59 +0200

    Add registration placeholder text and tailwind styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 const NavBar = () => (
-  <nav className="">
-    <ul className="flex flex-row justify-between">
-      <li>
-        <a href="home">Home</a>
-      </li>
-      <li>
-        <a href="about">About</a>
-      </li>
-      <li>
-        <a href="competition">Competition</a>
-      </li>
-    </ul>
+  <nav className="bg-black">
+    <div className="flex justify-between text-white items-center p-4 text-base">
+      <p className="text-white">Login/Register</p>
+      <ul className="flex gap-3">
+        <li className="">
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="competition">Competition</a>
+        </li>
+      </ul>
+    </div>
+
   </nav>
 );
 
