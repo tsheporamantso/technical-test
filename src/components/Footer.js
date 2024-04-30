6106a04dd95292734067c44435d4cda593d5ba26
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-30 17:51:39 +0200

    Add footer JSX

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { FaLinkedinIn, FaGithub, FaAngellist } from 'react-icons/fa';
 
 const Footer = () => (
-  <div>Footer</div>
+  <footer>
+    <a id="footer__log" href="home">LESEGO</a>
+    <div id="footer__socials">
+      <a href="facebook" target="blank" rel="noreferrer" aria-label="LinkedIn"><FaLinkedinIn /></a>
+      <a href="facebook" target="blank" rel="noreferrer" aria-label="GitHub"><FaGithub /></a>
+      <a href="facebook" target="blank" rel="noreferrer" aria-label="Angellist"><FaAngellist /></a>
+    </div>
+    <div id="footer__copyright">
+      <small>&copy Lesego Putsoa. All rights reserved </small>
+    </div>
+  </footer>
 );
 
 export default Footer;
