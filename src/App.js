855c4e9a20049e5580266b82ceedd642acc019db
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-01 15:40:07 +0200

    Conditionally render layout and add Sign up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
 
-const App = () => (
-  <>
-    <NavBar />
-    <Footer />
-    <SignUp />
-  </>
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        {loggedIn ? <Route path="/layout" element={<Layout />} /> : null}
+        <Route path="footer" element={<Footer />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
