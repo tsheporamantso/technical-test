76ce5301e1a246c379eb19a0cb2aaf9b4a6cce37
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-01 11:52:13 +0200

    Add h1 logo on navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => (
   <nav className="bg-black">
     <div className="flex justify-between text-white items-center p-4 text-base">
-      <p className="text-white">Login/Register</p>
+      <h1 className="text-cyan-500 uppercase">Lesego</h1>
       <ul className="flex gap-3">
         <li className="">
           <a href="home">Home</a>
