de1b67258a059a9d3528504b3f9badc885d5580b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-01 13:40:56 +0200

    Import sign up component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import SignUp from './pages/SignUp';
 
 const App = () => (
   <>
     <NavBar />
     <Footer />
+    <SignUp />
   </>
 
 );
