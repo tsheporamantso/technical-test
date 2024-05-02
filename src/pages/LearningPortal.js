6174114db8eecc44491d04d6928d316ef247130c
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-02 10:39:51 +0200

    Add LearningPortal component to Layout

---
+++ src/pages/LearningPortal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LearningPortal = () => (
+  <div id="learning__container" className="bg-cover bg-center bg-no-repeat h-screen">LearningPortal</div>
+);
+
+export default LearningPortal;
